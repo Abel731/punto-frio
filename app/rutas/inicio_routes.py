17d8b152f5dac48b5e43fb58eb9c9dc1b52c52df
Abel731 <abelp9289@gmail.com>
2026-07-16 17:48:10 -0300

    mas cambios

--- app/rutas/inicio_routes.py
+++ app/rutas/inicio_routes.py
@@ -3,5 +3,5 @@ from flask import Blueprint, redirect, url_for
 bp_inicio = Blueprint("inicio", __name__)
 
 @bp_inicio.get("/")
-def inicio():
+def landing_page():
     return redirect(url_for("index.html"))
